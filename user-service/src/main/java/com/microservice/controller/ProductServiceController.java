23cc51ea414db70ad280f13e8f2a72cd98461fd6
Tsumbedzo_Nevhutalu <vanessatn96@gmail.com>
2023-06-19 14:28:06 +0200

    An eclipse with basic scaffold and basic http requirest and the application.properties file

---
+++ user-service/src/main/java/com/microservice/controller/ProductServiceController.java
@@ -0,0 +1,18 @@
+package com.microservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductServiceController {
+	
+	@GetMapping(value = "hi")
+		public String Greeting() {
+		
+		return "HelloWorld";
+	}
+	
+
+}
